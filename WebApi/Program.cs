07698abe17c21eb973eb6ac1c36742e517082275
Djordje Ljubicic <ljubicic.dj@gmail.com>
2024-05-14 21:49:29 +0200

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,38 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
