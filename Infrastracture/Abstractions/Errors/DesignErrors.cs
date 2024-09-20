6756a1757bf944d3c928a425ba9716a6069a6da6
Đorđe Ljubičić <djordje.ljubicic@nites.eu>
2024-09-20 14:07:02 +0200

    feat design error

---
+++ Infrastracture/Abstractions/Errors/DesignErrors.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Abstractions.Errors;
+
+namespace Infrastracture.Abstractions.Errors
+{
+    public static class DesignErrors
+    {
+        public static readonly Error NotFound = new Error(
+                 "Design.NotFound",
+                 "Dizajn nije pronađen");
+    }
+}
