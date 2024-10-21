1c428fe72d8e053221c994033c3371558e5eda3d
Đorđe Ljubičić <djordje.ljubicic@nites.eu>
2024-10-21 15:16:22 +0200

    feat: added constants class for product details

---
+++ Application/Constants/Product.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Application.Constants
+{
+    public static class Product
+    {
+        //product
+        public const int productId = 1;
+
+        //variants
+        public const int in11x14 = 14125;
+        public const int in16x23 = 19528;
+        public const int in20x30 = 16365;
+    }
+}
