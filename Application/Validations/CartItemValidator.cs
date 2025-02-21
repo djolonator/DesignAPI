66e0edb982e2e3cb16fae82c37442b2b0e345c33
Đorđe Ljubičić <djordje.ljubicic@nites.eu>
2025-02-21 16:28:30 +0100

    validator fix

--- Application/Validations/CartItemValidator.cs
+++ Application/Validations/CartItemValidator.cs
@@ -8,8 +8,8 @@ namespace Application.Validations
     {
         public CartItemValidator() 
         {
-            RuleFor(item => item.DesignId).GreaterThan(0).LessThan(100);
-            RuleFor(item => item.ProductId).GreaterThan(0).LessThan(5);
+            RuleFor(item => item.DesignId).GreaterThan(0);
+            RuleFor(item => item.ProductId).GreaterThan(0);
             RuleFor(item => item.Quantity).GreaterThan(0).LessThan(10);
         }
     }
